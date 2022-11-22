d403baf9b3f3054e52b7fdd18323b21dd01cfc23
orxan97 <orkhanna@code.edu.az>
2022-11-22 17:05:04 +0400

    Add project files.

---
+++ 43t4t4ttyret/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
+
+namespace _43t4t4ttyret
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+             metod(num);
+        }
+
+        static void metod(int num)
+        {
+            
+            if (num % 7 == 0)
+            {
+                Console.WriteLine("eded 7-e bolunur");
+            }
+            else
+            {
+               if (num % 7 <= 3)
+                {
+
+                    Console.WriteLine(num -(num%7));
+                }
+
+                   else if(num% 7 >= 4)
+                {
+                    Console.WriteLine((num - (num % 7)+7));
+                }
+                   
+               
+                    
+
+                }
+
+            }
+
+            
+           
+        }
+      
+    }
+   
